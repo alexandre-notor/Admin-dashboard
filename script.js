ff753b80d482a78a1a496a2e29ad06a78aa12fc3
M ash <ashuza1411@gmail.com>
2022-05-29 13:23:56 +0200

    Add top menu and man contents

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const body = document.querySelector("body");
     modeToggle = document.querySelector(".mode-toggle");
     sidbar = document.querySelector("nav");
     sidbarToggle = document.querySelector(".sidbar-toggle");
-    Image_Id = document.getElementById('getImage');
+    
 
 modeToggle.addEventListener("click", () =>{
     body.classList.toggle('dark');
@@ -14,22 +14,26 @@ sidbarToggle.addEventListener("click", () =>{
 
 
 // function logoChange() {
-//     var Image_Id = document.getElementById('getImage');
-//     if (Image_Id.src.match("Img/logo.png")) {
-//         Image_Id.src = "Img/logo1.png";
+//     let Image_Id2 = document.getElementById('getImage');
+
+//     if (Image_Id2.src.match("Img/logo.png")) {
+//         Image_Id2.src = "Img/logo1.png";
 //     }
 //     else {
-//         Image_Id.src = "Img/logo.png";
+//         Image_Id2.src = "Img/logo.png";
 //     }
 // } 
 
-function logoChange1() {
 
-    if (Image_Id.src.match("Img/logo.png")) {
-        Image_Id.src = "Img/logo2.png";
+function logoChange1() {
+    let  Image_Id1 = document.getElementById('getImage');
+
+    if (Image_Id1.src.match("Img/logo.png")) {
+        Image_Id1.src = "Img/logo2.png";
         
     }
     else {
-        Image_Id.src = "Img/logo.png";
+        Image_Id1.src = "Img/logo.png";
     }
 }  
+
