887adb40e760119b440eac71413c9e4a6a67482c
alexandre-notor <amanichambu48@gmail.com>
2022-05-29 18:28:53 +0200

    fix: Logo on Toggle

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
-const body = document.querySelector("body");
-    modeToggle = document.querySelector(".mode-toggle");
-    sidbar = document.querySelector("nav");
-    sidbarToggle = document.querySelector(".sidbar-toggle");
+const body = document.querySelector("body"), modeToggle = document.querySelector(".mode-toggle"), sidbar = document.querySelector("nav"), sidbarToggle = document.querySelector(".sidbar-toggle");
     
 
 modeToggle.addEventListener("click", () =>{
     body.classList.toggle('dark');
+    logoChange();
 });
 
 sidbarToggle.addEventListener("click", () =>{
     sidbar.classList.toggle("close");
+    logoChange();
 });
 
+function isDark() {
+    if (body.className.indexOf('dark') < 0) return false;
+    return true;
+}
+function isBig() {
+    if (sidbar.className.indexOf('close') < 0) return true;
+    return false;
+}
 
-// function logoChange() {
-//     let Image_Id2 = document.getElementById('getImage');
-
-//     if (Image_Id2.src.match("Img/logo.png")) {
-//         Image_Id2.src = "Img/logo1.png";
-//     }
-//     else {
-//         Image_Id2.src = "Img/logo.png";
-//     }
-// } 
-
-
-function logoChange1() {
-    let  Image_Id1 = document.getElementById('getImage');
-
-    if (Image_Id1.src.match("Img/logo.png")) {
-        Image_Id1.src = "Img/logo2.png";
-        
-    }
-    else {
-        Image_Id1.src = "Img/logo.png";
-    }
-}  
-
+function logoChange() {
+    let Image_Id2 = document.getElementById('getImage');
+    if(!isBig()) 
+        Image_Id2.src = "Img/logo2.png";
+    else if (isBig() && isDark())
+        Image_Id2.src = "Img/logo1.png";
+    else
+        Image_Id2.src = "Img/logo.png";
+} 
